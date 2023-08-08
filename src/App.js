70bdb87af5724bcf3ade9498f63d600936b727cd
medoapoali <medoapoali@gmail.com>
2023-08-08 13:37:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+export default function App() {
+  return (
+    <div>
+        <header>medoapoali</header>
+        <h1>medoapoali</h1>
+        <h1>medoapoali</h1>
+        <h1>medoapoali</h1>
+        <p>update</p>
+        <p>react</p>
+    </div>
+  );
+}
